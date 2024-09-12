9f40719c128e4ff6839a0f7a69a14e0d89ad90fc
E1ecTro5 <abdulkerimovr7@gmail.com>
2024-09-12 13:45:02 +0400

    Debug class added.
    Debug.Log and Debug.CommandLog to show some info on console.

---
+++ Project3/Debug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Project3
+{
+    /// <summary>
+    /// Class for writing needed information to the console.
+    /// </summary>
+    public static class Debug
+    {
+        //Default message
+        public static void Log(Message message)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"[{dateTime}] {message.Chat.FirstName} | {message.Text}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        //Command use
+        public static void CommandLog(Message message, string commandName)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[{dateTime}] {message.Chat.FirstName} used {commandName}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
