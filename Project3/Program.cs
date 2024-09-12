fdb3e1ba05237af2d30333bdc86818b5edc0befb
E1ecTro5 <abdulkerimovr7@gmail.com>
2024-09-12 12:02:27 +0400

    Project setup.
    All needed thing for starting.

---
+++ Project3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
